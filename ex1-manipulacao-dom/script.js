67541354ad08aabc180eb6cfa88accdd7eff7ab1
GustavoSantanaRe <gustavo.rebolo@aluno.senai.br>
2026-02-26 16:20:52 -0300

    codigo 5B completo

--- ex1-manipulacao-dom/script.js
+++ ex1-manipulacao-dom/script.js
@@ -1,8 +1,10 @@
 // Seleciona o elemento do botão no DOM usando o ID "myButton" e armazena na variável 'button'
 
-// 3ª Digitação (Aqui) 
+// 3ª Digitação (Aqui)  
+const button = document. querySelector("#mybutton");
 
 // Seleciona o elemento de parágrafo no DOM usando o ID "myText" e armazena na variável 'text'
+const button = document. querySelector("#myText")
 
-
-// Adiciona um "ouvinte" de evento ao botão. Quando o botão for clicado, a função dentro do 'addEventListener' será executada
+// Adiciona um "ouvinte" de evento ao botconão. Quando o botão for clicado, a função dentro do 'addEventListener' será executada
+button.addEventListener(click), () 
